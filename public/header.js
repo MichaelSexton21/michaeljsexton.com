314a2b9e10e7d724d7c3df5699de8288872693ce
Michael Sexton <ms3648@cornell.edu>
2024-03-26 11:13:19 -0400

    updated icon for dark mode

--- public/header.js
+++ public/header.js
@@ -29,3 +29,39 @@ function setupNavbar() {
     });
   });
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  function changeFavicon() {
+    const theme = window.matchMedia("(prefers-color-scheme: dark)").matches
+      ? "dark"
+      : "light";
+    const faviconImage = document.getElementById("navbar-favicon");
+    if (faviconImage) {
+      faviconImage.src =
+        theme === "dark"
+          ? "assets/favicon_white.ico"
+          : "assets/favicon_black.ico";
+    } else {
+      console.error('Element with id "navbar-favicon" not found.');
+    }
+  }
+
+  setTimeout(() => {
+    changeFavicon();
+  }, 500);
+
+  window
+    .matchMedia("(prefers-color-scheme: dark)")
+    .addEventListener("change", changeFavicon);
+});
+
+// Initial favicon setup based on user's preferred theme
+const userPrefersDark =
+  window.matchMedia &&
+  window.matchMedia("(prefers-color-scheme: dark)").matches;
+changeFavicon(userPrefersDark ? "dark" : "light");
+
+// Listener for theme change
+window.matchMedia("(prefers-color-scheme: dark)").addListener((e) => {
+  changeFavicon(e.matches ? "dark" : "light");
+});
--- public/index.js
+++ public/index.js
@@ -28,3 +28,28 @@ document.addEventListener("DOMContentLoaded", loadContent);
 
 // Load new content when URL hash changes
 window.addEventListener("hashchange", loadContent);
+
+document.addEventListener("DOMContentLoaded", function () {
+  function changeFavicon() {
+    const theme = window.matchMedia("(prefers-color-scheme: dark)").matches
+      ? "dark"
+      : "light";
+    const faviconTitle = document.getElementById("title-favicon");
+    if (faviconTitle) {
+      faviconTitle.href =
+        theme === "dark"
+          ? "assets/favicon_white.ico"
+          : "assets/favicon_black.ico";
+    } else {
+      console.error('Element with id "navbar-favicon" not found.');
+    }
+  }
+
+  setTimeout(() => {
+    changeFavicon();
+  }, 500);
+
+  window
+    .matchMedia("(prefers-color-scheme: dark)")
+    .addEventListener("change", changeFavicon);
+});
